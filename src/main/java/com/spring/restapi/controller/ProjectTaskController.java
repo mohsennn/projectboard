8d76e71bac29efc21101204a5d4609c8dc370040
Mohsen <mohsenghanem02@gmail.com.com>
2020-04-23 23:05:16 +0200

    add delete method

--- src/main/java/com/spring/restapi/controller/ProjectTaskController.java
+++ src/main/java/com/spring/restapi/controller/ProjectTaskController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,6 +41,12 @@ public class ProjectTaskController {
 	public ResponseEntity<?> getById(@PathVariable Long pt_id) {
 		ProjectTask projectTask = projectTaskService.findById(pt_id);
 		return new ResponseEntity<ProjectTask>(projectTask, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{pt_id}")
+	public ResponseEntity<?> deleteProjectTask(@PathVariable Long pt_id) {
+		return new ResponseEntity<String>("Project task deleted ", HttpStatus.OK);
+
 	}
 
 }
