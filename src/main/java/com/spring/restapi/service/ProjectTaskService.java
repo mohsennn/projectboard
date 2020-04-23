1a522e6425eab20cceae776b21d2d90c2cb65d7f
Mohsen <mohsenghanem02@gmail.com.com>
2020-04-23 21:23:17 +0200

    gestion des exceptions pour le controleur

--- src/main/java/com/spring/restapi/service/ProjectTaskService.java
+++ src/main/java/com/spring/restapi/service/ProjectTaskService.java
@@ -2,23 +2,23 @@ package com.spring.restapi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import com.spring.restapi.model.ProjectTask;
 import com.spring.restapi.repository.ProjectTaskRepository;
 
-@Repository
-public class ProjectTaskService {
+@Service
+public class ProjectTaskService  {
 
 	@Autowired
 	private ProjectTaskRepository  projectTaskRepository;
 
-	public ProjectTask saveOrUpdateProjectTask(ProjectTask projectTask) {
-		
-		if(projectTask.getStatus()==null ||  projectTask.getStatus()=="") {
-			projectTask.setStatus("TO_DO");
-			}
-		
-		return projectTaskRepository.save(projectTask);
-	}
+	 public ProjectTask saveOrUpdateProjectTask(ProjectTask projectTask){
+
+	        if(projectTask.getStatus()==null || projectTask.getStatus()==""){
+	            projectTask.setStatus("TO_DO");
+	        }
+	        return projectTaskRepository.save(projectTask);
+	    }
 	
 }
